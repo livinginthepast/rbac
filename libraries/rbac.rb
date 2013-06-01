7dd42116e7cbcba7f279413b8d5c1e35004712b6
Eric Saxby <sax@livinginthepast.org>
2013-06-01 13:40:05 -0700

    Add comment around top-level RBAC module

--- libraries/rbac.rb
+++ libraries/rbac.rb
@@ -1,3 +1,8 @@
+# This module is used to retain state during the course of a chef
+# run. The LWRPs in the cookbook modify a global hash in this module,
+# and at the end of the chef run if user authorizations change they
+# are written out into the system.
+#
 module RBAC
   def self.authorizations
     @authorizations ||= {}
